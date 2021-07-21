fe5b53254766c2dbfa7336917c96d3251544a0c9
AlexDarchuk <o.darchuk@gmail.com>
2021-07-21 15:03:03 +0300

    editing comments

--- homework-1/task-1/main.js
+++ homework-1/task-1/main.js
@@ -3,12 +3,20 @@ const stairsBtn = document.querySelector('.stairs-btn');
 const showStairs = document.querySelector('.show-stairs');
 const lastStairs = document.querySelector('.lastStairs');
 
-let str = '$';
+let currencySymbol = '$';
 let rows = 0;
 let count = 0;
 
 stairsBtn.addEventListener('click', () => {
     let input = +inputNumber.value;
+    lastStairs.innerHTML = '';
+    
+    if (!input) {
+        stairsBtn.value = 'Enter';
+    }else {
+        stairsBtn.value = 'Clear';
+    }
+
     for(let i = 1; count < input; i++) {
         count += i;
         if(count <= input){
@@ -18,19 +26,24 @@ stairsBtn.addEventListener('click', () => {
             break;
         }
 }
+
     renderStr(rows,count,input);
     inputNumber.value = '';
+    input = 0;
+    rows = 0;
+    count = 0;  
 });
+
 
 function renderStr (rows,count,input){  
     let lastRow = input - count;
-    let totalStr = str;
+    let totalStr = currencySymbol;
 
     for(let i = 0; i < rows; i++){
         let p = document.createElement('p');
         p.innerText = totalStr;
         lastStairs.append(p);
-        totalStr += str;
+        totalStr += currencySymbol;
     }
 
     if(lastRow > 0){                                       
@@ -39,13 +52,14 @@ function renderStr (rows,count,input){
         p.innerText = totalStr;
         lastStairs.append(p);
     } 
-    showStairs.innerText = `Total rows  = ${rows}`;  
+    showStairs.innerText = `Total rows  = ${rows}`;
+    totalStr.innerHTML = '' 
 }
 
 function showLastRow (lastRow){
-    let result = str;
+    let result = currencySymbol;
     for(let i = 1; i < lastRow;i++){
-        result += str;
+        result += currencySymbol;
     }
     return result;
 }
